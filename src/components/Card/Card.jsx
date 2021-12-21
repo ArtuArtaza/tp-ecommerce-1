857b64ac7efcc01e51493dde1cf5032e9bcf2ce3
Arturo Iván Artaza <aartazaesc@gmail.com>
2021-12-20 21:44:39 -0300

    changes in Card component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -7,8 +7,11 @@ const Card = ({
             <img src={product.image} className="card-img-top" alt="product image" />
             <div className="card-body">
                 <h5 className="card-title">{product.title}</h5>
+                <p class="card-text">{product.description}</p>
+                <span className="text-success">{product.price}</span>
             </div>
         </div>
     )
 }
 
+export default Card
